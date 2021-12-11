4d31b7ed0c98794a2a051caa8cb7d90ab7a400cf
Alcides Fonseca <me@alcidesfonseca.com>
2021-12-11 09:08:42 +0000

    Day 11

---
+++ 11/b.lean
@@ -0,0 +1,66 @@
+def parseMatrix (lines : List String) : (Array (Array Nat)) :=
+  let parseLine := λx => (x.toList.map (λx => x.toNat - '0'.toNat)).toArray
+  let l := lines.map parseLine
+  l.toArray
+
+def size (m: Array (Array Nat)) : Nat × Nat :=
+  (m.size, m[0].size)
+
+def contains10 (matrix: Array (Array Nat)) : Bool :=
+  matrix.any (λx => x.any (λy => y == 10))
+
+def replacePos (matrix: Array (Array Nat)) (i j v:Nat) : Array (Array Nat) :=
+  let newLine := matrix[i].set! j v
+  matrix.set! i newLine
+
+
+def neighboursInc (matrix: Array (Array Nat)) (i j:Nat) : Array (Array Nat) := do
+  let mut m := matrix
+  m := if i > 0 then replacePos m (i-1) j (m[i-1][j]+1) else m
+  m := if i < 9  then replacePos m (i+1) j (m[i+1][j]+1) else m
+  m := if j > 0 then replacePos m i (j-1) (m[i][j-1]+1) else m
+  m := if j < 9 then replacePos m i (j+1) (m[i][j+1]+1) else m
+  m := if i > 0 && j > 0 then replacePos m (i-1) (j-1) (m[i-1][j-1]+1) else m
+  m := if i > 0 && j < 9 then replacePos m (i-1) (j+1) (m[i-1][j+1]+1) else m
+  m := if i < 9 && j > 0 then replacePos m (i+1) (j-1) (m[i+1][j-1]+1) else m
+  m := if i < 9 && j < 9 then replacePos m (i+1) (j+1) (m[i+1][j+1]+1) else m
+  m
+
+partial def iterate (matrix: Array (Array Nat)) (positions: Array (Nat × Nat)) (acc:Nat) : Nat × Array (Array Nat) × Bool := do
+  let mut m := matrix
+  let mut f := acc
+  let mut npos := #[]
+  for i in [:10] do
+    for j in [:10] do
+      let change := m[i][j] >= 10 && not (positions.contains (i,j))
+      m := if change then neighboursInc m i j else m
+      f := if change then f + 1 else f
+      npos := if change then npos.push (i, j) else npos
+  --dbgTrace ( (npos.size.toSuperscriptString) ++ " " ++ (positions.size.toSuperscriptString) ) $ fun _ =>
+  if npos.size + positions.size == 100 then 
+      (f, m, true)
+  else if npos.size > 0 then
+    iterate m (positions ++ npos) f
+  else (f, m, false )
+
+def reset (matrix: Array (Array Nat)) : Array (Array Nat) := 
+  matrix.map ( λx => x.map (λy => if y > 9 then 0 else y)  )
+
+def advance (matrix: Array (Array Nat)) : Array (Array Nat) := 
+  matrix.map ( λx => x.map (λy => if y <= 9 then y+1 else y)  )
+
+def main (args : List String) : IO UInt32 :=
+do
+  let input ← IO.FS.lines "input.txt"
+  let mut matrix := parseMatrix input.toList
+  for i in [:10000] do
+    let m := advance matrix
+    let (fl, m, e) := iterate m #[] 0
+    matrix := reset m
+    if e then
+      IO.println $ (i+1)
+      break
+    else
+      pure ()
+
+  return 0
